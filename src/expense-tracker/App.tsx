47afa2fa68aaf71699920a71bdb3d3b394e97e5e
Fernanda Ramírez <mframirezfuentes@gmail.com>
2023-07-10 18:37:32 -0400

    Modularizacion del codigo, creando metodos para las llamadas Rest

---
+++ src/expense-tracker/App.tsx
@@ -0,0 +1,39 @@
+import { useState } from "react";
+import ExpenseList from "./components/ExpenseList";
+import ExpenseFilter from "./components/ExpenseFilter";
+import ExpenseForm from "./components/ExpenseForm";
+import categories from "./categories";
+
+function App() {
+  const [selectedCategory, setSelectedCategory] = useState("");
+  const [expenses, setExpenses] = useState([
+    { id: 1, description: "aaa", amount: 10, category: "Utilities" },
+    { id: 2, description: "dsf", amount: 15, category: "Groceries" },
+    { id: 3, description: "agfda", amount: 20, category: "Utilities" },
+    { id: 4, description: "ewrwe", amount: 12, category: "Home" },
+    { id: 5, description: "fdgfd", amount: 17, category: "Entertainment" },
+  ]);
+
+  const visibleExpenses = selectedCategory
+    ? expenses.filter((e) => e.category === selectedCategory)
+    : expenses;
+
+  return (
+    <div>
+      <div className="mb-5">
+        <ExpenseForm onSubmit={expense => setExpenses([...expenses,{...expense, id:expenses.length+1}])} />
+      </div>
+      <div className="mb-3">
+        <ExpenseFilter
+          onSelectCategory={(category) => setSelectedCategory(category)}
+        />
+      </div>
+
+      <ExpenseList
+        expenses={visibleExpenses}
+        onDelete={(id) => setExpenses(expenses.filter((e) => e.id !== id))}
+      />
+    </div>
+  );
+}
+export default App;
