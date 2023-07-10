5080f50f9210ae1a7097c25741dcb95fa2707599
Fernanda Ramírez <mframirezfuentes@gmail.com>
2023-07-10 19:11:47 -0400

    Create a hook of user

--- src/App.tsx
+++ src/App.tsx
@@ -1,27 +1,10 @@
 import { useEffect, useState } from "react";
 import { CanceledError } from "./services/api-client";
 import userService, { User } from "./services/userService";
+import useUser from "./hooks/useUser";
 
 function App() {
-  const [users, setUsers] = useState<User[]>([]);
-  const [error, setError] = useState("");
-  const [isLoading, setLoading] = useState(false);
-  useEffect(() => {
-    setLoading(true);
-    const { request, cancel } = userService.getAll<User>();
-    request
-      .then((response) => {
-        setUsers(response.data);
-        setLoading(false);
-      })
-      .catch((error) => {
-        if (error instanceof CanceledError) return;
-        setError(error.message);
-        setLoading(false);
-      });
-
-    return () => cancel();
-  }, []);
+  const { users, error, isLoading, setError, setUsers } = useUser;
 
   const deleteUser = (user: User) => {
     const originalUsers = [...users];
